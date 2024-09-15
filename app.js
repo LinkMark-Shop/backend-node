ba49da83cb3b348eb558a65eac7597a6991be15a
leonardoTavaesM <leomaltadesa0712@gmail.com>
2024-09-15 03:20:52 -0300

    feat: adding cors

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 require("dotenv").config({ path: ".env.local" });
 const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Configuração do CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Permitir apenas uma origem específica
+    methods: "GET,POST,PUT,DELETE", // Métodos permitidos
+    allowedHeaders: "Content-Type,Authorization", // Cabeçalhos permitidos
+  })
+);
+
 const productRoutes = require("./routes/productRoutes");
 // const userRoutes = require("./routes/userRoutes");
 // const authorRoutes = require("./routes/authorRoutes");
 
-const app = express();
 app.use(express.json());
 
 // Rotas
